09873759372500c669b7adf22e2af054213d9496
akmalmaulanak <akmalkismoyo@gmail.com>
2023-10-23 18:25:15 +0700

    membuat tampilan login dan register saya mengerjakan bersama alan nurohim

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('authenticate') }}">
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+<div class="container" style="border-radius: 20px; background-color: white; box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.3);">
+    <div class="row" style="margin: 50px 0 50px;">
+        <div class="col-md-6" style="margin-top: 50px;">
+            <div class="appearance">
+                <img src="../asset/image.svg" alt="Gambar" style="width: 100%; height: 100%;">
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="row-content">
+                <div class="card" style=" background-color: #505ABB; color: white; border-radius: 20px;">
+                <div class="title" style="text-align: center; margin-top: 30px;">
+                <h2>Login</h2>
+                <a>Please enter your Login and your Password</a>
+                </div>
+                
+                <div class="card-body" >
+                    <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" placeholder= "Username" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +39,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password"placeholder="Password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -53,7 +63,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" style="height: 25%; width: 73%; background-color: #4CAF50;">
                                     {{ __('Login') }}
                                 </button>
 
@@ -65,17 +75,19 @@
 
                                 <br><br>
                         <b>Atau Login dengan akun:</b><br><br>
-                        <a class="ml-1 btn btn-primary btn-sm" href="{{ url('auth/google') }}" style="margin-top: 0px !important;background: white; color: #ffffff;padding: 5px;border-radius:7px;" id="btn-glogin">
-                        <i class="fab fa-google" aria-hidden="true"></i>&nbsp;<img src="google.png" width="20px" height="auto" alt=""></a>
+                        <a class="ml-1 btn btn-primary btn-sm" href="{{ url('auth/google') }}" style="margin-top: 0px !important;background: rgb(243, 5, 5);color: #ffffff;padding: 5px;border-radius:7px;" id="btn-glogin">
+                        <i class="fab fa-google" aria-hidden="true"></i>&nbsp;Google</a>
 
-                        <a class="ml-1 btn btn-primary btn-sm" href="{{ url('auth/twitter') }}" style="margin-top:0px !important;background: white; color:#ffffff; padding:5px; border-radius:7px" id="btn-twlogin">
-                            <i class="fab fa-twitter" aria-hidden="true"></i>&nbsp;<img src="twitter.png" width="20px" height="auto" alt=""></a>
+                        <a class="ml-1 btn btn-primary btn-sm" href="{{ url('auth/twitter') }}" style="margin-top:0px !important;background: rgb(29, 161, 242); color:#ffffff; padding:5px; border-radius:7px" id="btn-twlogin">
+                            <i class="fab fa-twitter" aria-hidden="true"></i>&nbsp;Twitter</a>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-    </div>
 </div>
 @endsection
+
+
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<link rel="stylesheet" href="{{ asset('css/register.css') }}">
+<div class="container" style="border-radius: 20px; background-color: white; box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.3);">
+    <div class="row" style="margin: 50px 0 50px;">
+    <div class="col-md-6" style="margin-top: 50px;">
+            <div class="appearance">
+                <img src="../asset/image.svg" alt="Gambar" style="width: 100%; height: 100%;">
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card" style="background-color: #505ABB; color: white; border-radius: 20px;">
+            <div class="title" style="text-align: center; margin-top: 30px;">
+                <h2>Register</h2>
+                <a>Please enter your Name, Login and your Password</a>
+                </div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('store') }}">
+                    <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                            <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" placeholder="Username" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +39,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -43,7 +53,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" placeholder="Password"class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -57,13 +67,13 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control"placeholder="Re-enter Password" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" style="height: 100%; width: 100%; background-color: #4CAF50;">
                                     {{ __('Register') }}
                                 </button>
                             </div>
